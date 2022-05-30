213f8f1f3ca6c1ba52609096aba7ada4cd8c2239
Joseph Cobbinah Ofori <josephcobbinah.jc@gmail.com>
2022-05-30 19:56:46 +0000

    storing users in localstorage

--- main.js
+++ main.js
@@ -3,32 +3,42 @@ const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
 const msg = document.querySelector('.msg');
 const users = document.querySelector('#users');
+let userList = [];
 
 myForm.addEventListener('submit', onSubmit);
 
+//load list on page load
+displayList();
+
 function onSubmit(e) {
     e.preventDefault();
-
-    const userList = [];
-
-    if(nameInput.value === '' || emailInput.value === '') {
+    if (nameInput.value === '' || emailInput.value === '') {
         msg.innerHTML = 'Please fill all fields';
         msg.classList.add('error');
-
-        setTimeout(() => {msg.remove()}, 3000)
+        setTimeout(() => { msg.remove() }, 3000)
     } else {
-        const li = document.createElement('li');
-        li.appendChild(document.createTextNode(`${nameInput.value} 
-        : ${emailInput.value}`));
-
-        users.appendChild(li);
-
-
-        // nameInput.value = '';
-        // emailInput.value = '';
-
+        //add the to the list and store in localStorage
+        addUser();
     }
+}
 
-    userList.push({"name":nameInput.value, "email":emailInput.value});
-    localStorage.setItem("userList":JSON.stringify(userList));
+function addUser() {
+    userList.push({ name: nameInput.value, email: emailInput.value });
+    localStorage.setItem("userList", JSON.stringify(userList))
+    nameInput.value = '';
+    emailInput.value = '';
+    displayList();
+}
+
+function displayList() {
+    if(localStorage.getItem("userList")!=null){
+        userList = JSON.parse(localStorage.getItem("userList"))
+    }
+    users.innerHTML = '';
+    for (let i = 0; i < userList.length; i++) {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${userList[i].name} 
+        : ${userList[i].email}`));
+        users.appendChild(li);
+    }
 }
